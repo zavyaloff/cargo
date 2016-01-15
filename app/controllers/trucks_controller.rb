265cd8f2ffdc0bf7d84dcb9368dd91e4d7e1e38c
Zavyaloff <antonio.zavyaloff@gmail.com>
2016-01-15 04:20:36 +0300

    Introduce Truck MVC Scaffold

---
+++ app/controllers/trucks_controller.rb
@@ -0,0 +1,74 @@
+class TrucksController < ApplicationController
+  before_action :set_truck, only: [:show, :edit, :update, :destroy]
+
+  # GET /trucks
+  # GET /trucks.json
+  def index
+    @trucks = Truck.all
+  end
+
+  # GET /trucks/1
+  # GET /trucks/1.json
+  def show
+  end
+
+  # GET /trucks/new
+  def new
+    @truck = Truck.new
+  end
+
+  # GET /trucks/1/edit
+  def edit
+  end
+
+  # POST /trucks
+  # POST /trucks.json
+  def create
+    @truck = Truck.new(truck_params)
+
+    respond_to do |format|
+      if @truck.save
+        format.html { redirect_to @truck, notice: 'Truck was successfully created.' }
+        format.json { render :show, status: :created, location: @truck }
+      else
+        format.html { render :new }
+        format.json { render json: @truck.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /trucks/1
+  # PATCH/PUT /trucks/1.json
+  def update
+    respond_to do |format|
+      if @truck.update(truck_params)
+        format.html { redirect_to @truck, notice: 'Truck was successfully updated.' }
+        format.json { render :show, status: :ok, location: @truck }
+      else
+        format.html { render :edit }
+        format.json { render json: @truck.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /trucks/1
+  # DELETE /trucks/1.json
+  def destroy
+    @truck.destroy
+    respond_to do |format|
+      format.html { redirect_to trucks_url, notice: 'Truck was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_truck
+      @truck = Truck.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def truck_params
+      params.require(:truck).permit(:manufacturer, :truck_type, :model, :weight)
+    end
+end
---
+++ test/controllers/trucks_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class TrucksControllerTest < ActionController::TestCase
+  setup do
+    @truck = trucks(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:trucks)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create truck" do
+    assert_difference('Truck.count') do
+      post :create, truck: { manufacturer: @truck.manufacturer, model: @truck.model, truck_type: @truck.truck_type, weight: @truck.weight }
+    end
+
+    assert_redirected_to truck_path(assigns(:truck))
+  end
+
+  test "should show truck" do
+    get :show, id: @truck
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @truck
+    assert_response :success
+  end
+
+  test "should update truck" do
+    patch :update, id: @truck, truck: { manufacturer: @truck.manufacturer, model: @truck.model, truck_type: @truck.truck_type, weight: @truck.weight }
+    assert_redirected_to truck_path(assigns(:truck))
+  end
+
+  test "should destroy truck" do
+    assert_difference('Truck.count', -1) do
+      delete :destroy, id: @truck
+    end
+
+    assert_redirected_to trucks_path
+  end
+end
